b90f1d94af0c3808ca21d3e41b300e9dfa0757b5
anton <svirepa.a@mail.ru>
2019-12-15 02:50:17 +0300

    commit 2

---
+++ src/main/java/backend/config/AppConfig.java
@@ -0,0 +1,11 @@
+package backend.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "backend")
+public class AppConfig {
+}
