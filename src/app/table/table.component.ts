e556591cb592d0d3f3b9ce6988be8c633640fa71
anton <svirepa.a@mail.ru>
2019-12-17 16:00:52 +0300

    commit 8

--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -18,6 +18,9 @@ export class TableComponent implements OnInit {
   xmlPathStatus;
   newRow: Staff;
   tableStatus;
+  validName = /^[A-Z][a-z]{1,40}$/;
+  validEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  rowsSelected = [];
   constructor(private appService: AppService) { }
   ngOnInit() {
     this.addTitle = "Show form";
@@ -57,7 +60,23 @@ export class TableComponent implements OnInit {
         }
       }
     }
+    if (!this.validName.test(this.newRow.firstName)) {
+      this.savedStatus = "invalid first name";
+      this.tableStatus = "";
+      return;
+    }
+    if (!this.validName.test(this.newRow.lastName)) {
+      this.savedStatus = "invalid last name";
+      this.tableStatus = "";
+      return;
+    }
+    if (!this.validEmail.test(this.newRow.email)) {
+      this.savedStatus = "invalid email";
+      this.tableStatus = "";
+      return;
+    }
     this.staff.push(this.newRow);
+    this.rowsSelected.push(false);
     this.savedStatus = "Successfully added";
     this.tableStatus = "";
     this.newRow = Object.create(Staff.prototype);
@@ -66,8 +85,30 @@ export class TableComponent implements OnInit {
     if (this.staff.length <= 0) {
       this.tableStatus = "Table empty";
     } else {
-      this.staff.pop();
+      let falseCount = 0;
+      for (const row of this.rowsSelected) {
+        if (row === false) {
+          falseCount++;
+        }
+      }
+      if (falseCount === this.rowsSelected.length) {
+        return;
+      }
+      const newStaff = [];
+      for (let i = 0; i < this.rowsSelected.length; i++) {
+        if (this.rowsSelected[i] === false) {
+          newStaff.push(this.staff[i]);
+        }
+      }
+      for (let i = 0; i < this.staff.length - newStaff.length; i++) {
+        this.rowsSelected.pop();
+      }
+      for (let i = 0; i < this.rowsSelected.length; i++) {
+        this.rowsSelected[i] = false;
+      }
+      this.staff = newStaff;
     }
+    this.tableStatus = "Successfully deleted";
   }
   async saveXml() {
     try {
@@ -92,12 +133,18 @@ export class TableComponent implements OnInit {
         .then((data: Company) => {
           this.staff = data.staff;
           this.properties = Object.keys(this.staff[0]);
+          for (let i = 0; i < data.staff.length; i++) {
+            this.rowsSelected[i] = false;
+          }
         });
       this.xmlPathStatus = "Successfully loaded";
     } catch (exc) {
       console.log(exc);
       this.xmlPathStatus = "error occurred";
     }
+  }
+  selectRow(row) {
+    this.rowsSelected[row] = !this.rowsSelected[row];
   }
 
 }
